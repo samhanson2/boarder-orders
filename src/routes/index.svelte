abf60bd29baec05a95665871f6fb4e010b133b83
Samantha <hanson.s@rangiruru.school.nz>
2020-10-28 15:42:41 +1300

    v9

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,34 @@
   // creates session.cart
   const { session } = stores();
   session.cart = [];
+
+  let button = [
+    {
+      name: "Green",
+      colour: "body.style.backgroundColor = 'hsl(142, 85%, 38%, 0.5)';",
+      class: "button is-fullwidth is-success green"
+    },
+    {
+      name: "Blue",
+      colour: "body.style.backgroundColor = 'hsla(204, 86%, 53%, 0.5)';",
+      class: "button is-fullwidth is-info blue"
+    },
+    {
+      name: "Orange",
+      colour: "body.style.backgroundColor = 'rgba(255, 140, 0, 0.7)';",
+      class: "button is-fullwidth orange"
+    },
+    {
+      name: "Yellow",
+      colour: "body.style.backgroundColor = 'hsl(48, 100%, 67%, 0.8)';",
+      class: "button is-fullwidth is-warning yellow"
+    },
+    {
+      name: "Pink",
+      colour: "body.style.backgroundColor = 'pink';",
+      class: "button is-fullwidth pink"
+    }
+  ];
   // function for buttons to go to next page
   function nameAndGo(flat) {
     session.flat = flat;
@@ -51,54 +79,16 @@
 </style>
 
 <h1 class="title has-text-centered">Order Food</h1>
-<!-- coloured flat buttons for each flat  -->
-<div class="whitebox">
-  <button
-    onclick="body.style.backgroundColor = 'hsl(142, 85%, 38%, 0.5)';"
-    on:click={() => {
-      nameAndGo('Green');
-    }}
-    class="button is-fullwidth is-success green">
-    Green Flat
-  </button>
-</div>
-<div class="whitebox">
-  <button
-    onclick="body.style.backgroundColor = 'hsla(204, 86%, 53%, 0.5)';"
-    on:click={() => {
-      nameAndGo('Blue');
-    }}
-    class="button is-fullwidth is-info blue">
-    Blue Flat
-  </button>
-</div>
-<div class="whitebox">
-  <button
-    onclick="body.style.backgroundColor = 'rgba(255, 140, 0, 0.7)';"
-    on:click={() => {
-      nameAndGo('Orange');
-    }}
-    class="button is-fullwidth orange">
-    Orange Flat
-  </button>
-</div>
-<div class="whitebox">
-  <button
-    onclick="body.style.backgroundColor = 'hsl(48, 100%, 67%, 0.8)';"
-    on:click={() => {
-      nameAndGo('Yellow');
-    }}
-    class="button is-fullwidth is-warning yellow">
-    Yellow Flat
-  </button>
-</div>
-<div class="whitebox">
-  <button
-    onclick="document.body.style.backgroundColor = 'pink';"
-    on:click={() => {
-      nameAndGo('Pink');
-    }}
-    class="button is-fullwidth pink">
-    Pink Flat
-  </button>
-</div>
+<!-- loop though buttons and print -->
+{#each button as flat}
+  <div class="whitebox">
+    <button
+      onclick={flat.colour}
+      on:click={() => {
+        nameAndGo(flat.name);
+      }}
+      class={flat.class}>
+      {flat.name} Flat
+    </button>
+  </div>
+{/each}
